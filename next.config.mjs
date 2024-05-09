3e4238341be25a467f256b6ea594ba34ea0e7c78
Ark2044 <crce.9517.ecs@gmail.com>
2024-05-10 01:58:22 +0530

    second commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns:[
-            {
-                protocol: 'https',
-                hostname: '*.googleusercontent.com',
-            },
-        ],
-        domains: ['files.edgestore.dev'],
-    }
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.googleusercontent.com",
+      },
+    ],
+    domains: ["files.edgestore.dev"],
+  },
 };
 
 export default nextConfig;
--- src/app/layout.js
+++ src/app/layout.js
@@ -18,14 +18,14 @@ export default function RootLayout({ children }) {
     <html lang="en" className="scroll-smooth">
       <body className={roboto.className}>
         <main className="max-w-4xl mx-auto p-4">
-          <EdgeStoreProvider>
-            <AppProvider>
+          <AppProvider>
+            <EdgeStoreProvider>
               <Toaster />
               <Header />
               {children}
               <Footer />
-            </AppProvider>
-          </EdgeStoreProvider>
+            </EdgeStoreProvider>
+          </AppProvider>
         </main>
       </body>
     </html>
