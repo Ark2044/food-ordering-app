624dbb0439385a7c7ac6809e77eb311f6516f462
Ark2044 <crce.9517.ecs@gmail.com>
2024-05-10 14:06:09 +0530

    changed to route

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,14 +18,14 @@ export default function RootLayout({ children }) {
     <html lang="en" className="scroll-smooth">
       <body className={roboto.className}>
         <main className="max-w-4xl mx-auto p-4">
-          <AppProvider>
-            <EdgeStoreProvider>
+          <EdgeStoreProvider>
+            <AppProvider>
               <Toaster />
               <Header />
               {children}
               <Footer />
-            </EdgeStoreProvider>
-          </AppProvider>
+            </AppProvider>
+          </EdgeStoreProvider>
         </main>
       </body>
     </html>
